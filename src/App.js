69b674d5604019992dbe89e26d4713d1e3cecaf8
Yogesh S <yogesh@qoruz.com>
2024-09-14 15:57:58 +0530

    chrome extension project is done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { vocabulary } from "./vocabList";
+import "../src/App.css";
 
 const App = () => {
-  return <div>App</div>;
+  const randomNumber = Math.floor(
+    Math.random() * Math.floor(vocabulary.length)
+  );
+
+  return (
+    <div className="App">
+      <h1>{vocabulary[randomNumber].FIELD1}</h1>
+      <h3>{vocabulary[randomNumber].FIELD2}</h3>
+    </div>
+  );
 };
 
 export default App;
