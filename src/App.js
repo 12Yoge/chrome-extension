b4ca7991de4f8ee246b63769b8b52e1fb81d9aab
Yogesh S <yogesh@qoruz.com>
2024-09-14 16:20:31 +0530

    update readme is done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { vocabulary } from "./vocabList";
 import "../src/App.css";
 
 const App = () => {
-  const randomNumber = Math.floor(
-    Math.random() * Math.floor(vocabulary.length)
+  const [randomNumber, setRandomNumber] = useState(
+    Math.floor(Math.random() * vocabulary.length)
   );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRandomNumber(Math.floor(Math.random() * vocabulary.length));
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [vocabulary.length]);
 
   return (
     <div className="App">
-      <h1>{vocabulary[randomNumber].FIELD1}</h1>
-      <h3>{vocabulary[randomNumber].FIELD2}</h3>
+      <h1>
+        <span>Vocabulary Name:</span> {vocabulary[randomNumber].FIELD1}
+      </h1>
+      <h3>
+        <span>Description: </span>
+        {vocabulary[randomNumber].FIELD2}
+      </h3>
     </div>
   );
 };
